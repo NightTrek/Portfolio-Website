ecb6c0c4d0ff9408aeac0136b66250bcabcb32be
toneDef00 <daniels@dual4t.com>
2020-02-11 15:05:19 -0800

    Updates

---
+++ precache-manifest.e2aae04689b8e26bd9b5219f2b85a45e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c1352c8ef7e5d6ba3363b85ba12703",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "32eb60e91de40a000c97",
+    "url": "/Portfolio-Website/static/css/main.769a7f8c.chunk.css"
+  },
+  {
+    "revision": "006f9b14820fdfe7571b",
+    "url": "/Portfolio-Website/static/js/2.59672f36.chunk.js"
+  },
+  {
+    "revision": "4fbf4566bfa91046d7a549b21c1c800d",
+    "url": "/Portfolio-Website/static/js/2.59672f36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32eb60e91de40a000c97",
+    "url": "/Portfolio-Website/static/js/main.6ff459a6.chunk.js"
+  },
+  {
+    "revision": "cc0648379e07a6d5b11b",
+    "url": "/Portfolio-Website/static/js/runtime-main.55081b39.js"
+  },
+  {
+    "revision": "63cbadc67a1122db44654b73f63faba9",
+    "url": "/Portfolio-Website/static/media/ClimateFeel.63cbadc6.png"
+  },
+  {
+    "revision": "fb8ddac774be573cf011287f8277eb4c",
+    "url": "/Portfolio-Website/static/media/JWT-Demo.fb8ddac7.png"
+  },
+  {
+    "revision": "646efd69d69b541dc14c80a4d3c21020",
+    "url": "/Portfolio-Website/static/media/TriviaGame1.646efd69.png"
+  },
+  {
+    "revision": "a9381096476aae026e4331c468aaebb5",
+    "url": "/Portfolio-Website/static/media/wordGuessGame.a9381096.png"
+  }
+]);
