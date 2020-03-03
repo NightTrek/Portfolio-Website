8c23d6a03e3f0b58cfd31494c47670dc074afd6d
Daniel <Daniels@dual4t.com>
2020-03-02 17:55:52 -0800

    fixed page title

--- react-portfolio-frontend/src/components/ContactPage/ContactPage.jsx
+++ react-portfolio-frontend/src/components/ContactPage/ContactPage.jsx
@@ -9,8 +9,8 @@ const ContactPage = () =>{
         return (
             <div className={"flexRow"}>
                 <div className={"flexColumn contactPanel"}>
-                        <h2>I am actively Looking for opportunities So feel free to contact me Via the links below</h2>
-                        <h2>Feel free to email me at <span className={"email"}>Daniels@dual4t.com</span> or find me on social media</h2>
+                        <h2>I am actively looking for opportunities! Please feel free to contact me via the links below</h2>
+                        <h2>Email me at <span className={"email"}>Daniels@dual4t.com</span> or find me on social media</h2>
                         <a href={"https://github.com/ToneDef00"}><h2> Check out my Github </h2></a>
                         <a href={"https://www.linkedin.com/in/daniel-steigman-bb652186/"}><h2> Connect with me on Linkdin </h2></a>
                 </div>
